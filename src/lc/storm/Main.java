d09d7926df8d30747c969fcd9fdd90f7a110cba2
Mathias Storm <mathias@storm.lc>
2021-01-19 17:28:19 +0100

    replace useful code with dummy code to practice java streams

--- src/lc/storm/Main.java
+++ src/lc/storm/Main.java
@@ -2,23 +2,50 @@ package lc.storm;
 
 import lc.storm.store.*;
 
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Storefront myShop = new Storefront();
-        List<Article> articles = myShop.getAvailableArticles();
-        System.out.println("Welcome to JAMA Store, these are the games we offer!");
-        for(Article article : articles) {
-            System.out.println(article);
-        }
+        File[] roots = File.listRoots();
 
-        // just for practice, lets iterate over it using the bare iterator
-        for(Iterator<Article> it = articles.iterator(); it.hasNext(); ) {
-            Article article = it.next();
-            System.out.println(article);
+        try {
+            pickFile(roots);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
+    public static void pickFile(File[] files) throws IOException {
+        System.out.println("");
+        for(int i = 0; i < files.length; i++) {
+            System.out.println((i + 1) + ": "+files[i].getPath());
+        }
+        System.out.println("");
+        int choice = getChoiceFromStdin();
+        File chosenFile = files[choice - 1];
+        if(chosenFile.isFile()) {
+            // read file contents if txt
+            System.out.println("File contents:");
+            printFileContents(chosenFile);
+        }
+        else {
+            pickFile(Objects.requireNonNull(chosenFile.listFiles()));
+        }
+    }
+
+    private static void printFileContents(File f) throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(f));
+        for(String line = fileReader.readLine(); line != null; line = fileReader.readLine()) {
+            System.out.println(line);
+        }
+    }
+
+    public static int getChoiceFromStdin() throws IOException {
+        System.out.print("Bitte wählen Sie einen Ordner oder eine Datei: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = Integer.parseInt(br.readLine());
+        return result;
+    }
+
 }
